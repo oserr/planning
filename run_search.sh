a1acb42ccaa4cb71b3560c907122276d9f8ab3e7
Omar Serrano <oser520@gmail.com>
2017-04-16 21:41:02 -0700

    Fix comments; use depth limited instead of greedy search

--- run_search.sh
+++ run_search.sh
@@ -9,12 +9,12 @@ fi
 # Air Cargo Problems 1-3
 for p in 1 2 3; do
     # Search methods:
-    # 1 - BFS
-    # 2 - DFS
-    # 3 - Greedy Best Firsth Search
-    # 4 - A* with Ignore Preconditons heuristic
-    # 5 - A* with Level Sum heuristic
-    for s in 1 3 7 9 10; do
+    #  1 - BFS
+    #  3 - DFS
+    #  4 - Depth limited
+    #  9 - A* with Ignore Preconditons heuristic
+    # 10 - A* with Level Sum heuristic
+    for s in 1 3 4 9 10; do
         python run_search.py -p $p -s $s >> ${outfile} 
     done
 done
