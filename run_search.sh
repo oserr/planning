00898d0817147783606bc0df9f2dd6067afaa538
Omar Serrano <oser520@gmail.com>
2017-04-18 12:50:15 -0700

    Use UCS instead of depth limited search

--- run_search.sh
+++ run_search.sh
@@ -11,10 +11,10 @@ for p in 1 2 3; do
     # Search methods:
     #  1 - BFS
     #  3 - DFS
-    #  4 - Depth limited
+    #  5 - UCS
     #  9 - A* with Ignore Preconditons heuristic
     # 10 - A* with Level Sum heuristic
-    for s in 1 3 4 9 10; do
+    for s in 1 3 5 9 10; do
         python run_search.py -p $p -s $s >> ${outfile} 
     done
 done
